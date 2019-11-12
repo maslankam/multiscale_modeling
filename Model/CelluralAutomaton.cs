5fe92bdde48c59bd880835afd2016d3263eda919
maslankam <m.maslanka123@gmail.com>
2019-11-12 15:10:04 +0100

    Create CelluralAutomaton.cs

---
+++ Model/CelluralAutomaton.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Drawing;
+
+namespace Model{
+    public class CelluralAutomaton
+    {
+        private CelluralSpace space;
+        public CelluralAutomaton(){
+
+        }
+        
+        public void NextStep(){
+
+        }
+
+        public void RenderCurrentView(){
+
+        }
+
+        public void Reset(){
+
+        }
+
+    }
+
+    
+}
