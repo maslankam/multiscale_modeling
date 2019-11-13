4408f3c6e85418c1414d5f8d6e053e07addc4ee8
maslankam <m.maslanka123@gmail.com>
2019-11-13 11:13:08 +0100

    Create VonNeumanNeighborhoodTest.cs
    https://andrewlock.net/creating-parameterised-tests-in-xunit-with-inlinedata-classdata-and-memberdata/

---
+++ Test/VonNeumanNeighborhoodTest.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Xunit;
+
+using Model;
+
+namespace Test
+{
+    public class VonNeumanNeighborhoodTest
+    {
+        private static Cell a,b,c,d,e,f,g,h,i ;    
+        private static Cell[,] space = {
+                {a, b, c },
+                {c, d, e },
+                {f, g, h }
+            };
+
+        public VonNeumanNeighborhoodTest(){
+            a = new Cell();
+            b = new Cell();
+            c = new Cell();
+            d = new Cell();
+            e = new Cell();
+            f = new Cell();
+            g = new Cell();
+            h = new Cell();
+            i = new Cell();
+        }
+
+
+        [Theory]
+        [ClassData(typeof(AbsorbingTestData))]
+        public void AbsorbingTest()
+        {
+            Cell[] expectedNeighbours = { a, b, c, e, f, g, h };
+
+            var neighbours = VonNeumanNeighborhood.Neighbours(space , 1, 1, AbsorbingBoundary.BoundaryCondition());
+
+            Assert.NotEmpty(space);
+
+            Assert.Equal(neighbours, expectedNeighbours);
+        }
+
+        private class AbsorbingTestData : IEnumerable<object[]>
+        {   ///    0 1 2
+            /// 0 |a|b|c|
+            /// 1 |d|e|f|
+            /// 2 |g|h|i|
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[] { 1, 1, new Cell[]{b, d, f, h} }; //e - center
+                yield return new object[] { 0, 0, new Cell[]{b, d} }; //a
+                yield return new object[] { 0, 1, new Cell[]{a, e, c} }; //b
+                yield return new object[] { 0, 2, new Cell[]{b, f} }; //c
+                yield return new object[] { 1, 0, new Cell[]{a, e, g} }; //d
+                yield return new object[] { 1, 2, new Cell[]{c, e, i} }; //f
+                yield return new object[] { 2, 0, new Cell[]{d, h} }; //g
+                yield return new object[] { 2, 1, new Cell[]{e, g, i} }; //h
+                yield return new object[] { 2, 2, new Cell[]{h, f} }; //i
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+            }
+
+    }
+}
