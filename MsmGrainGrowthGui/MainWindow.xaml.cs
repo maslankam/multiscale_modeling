73012a6436448d3efd0dd51b4560a5ee53aa8c1a
chalwa <m.maslanka123@gmail.com>
2019-11-16 21:52:52 +0100

    Steps per click and variable number of grains added

--- MsmGrainGrowthGui/MainWindow.xaml.cs
+++ MsmGrainGrowthGui/MainWindow.xaml.cs
@@ -24,6 +24,7 @@ namespace MsmGrainGrowthGui
     /// </summary>
     public partial class MainWindow : Window
     {
+        //TODO: Add string validation this is really important!
         public string SpaceSize
         {
             get { return this._spaceSize.ToString(); }
@@ -42,16 +43,17 @@ namespace MsmGrainGrowthGui
             set {  this._step = value; }
         }
 
+        public string StepsPerClick
+        {
+            get { return this._stepsPerClick.ToString(); }
+            set { this._stepsPerClick = Convert.ToInt32(value); }
+        }
+
+        private int _stepsPerClick;
         private int _grainCount;
         private int _spaceSize;
         private CelluralAutomaton automaton;
-        private Grain redGrain;
-        private Grain blueGrain;
-        private Grain cyanGrain;
-        private Grain magentaGrain;
-        private Grain yellowGrain;
-        private Grain greenGrain;
-        private Grain darkGreenGrain;
+        private Grain[] grains;
         private Cell[,] space;
         private Cell[,] previousSpace;
         private int _step;
@@ -62,67 +64,61 @@ namespace MsmGrainGrowthGui
             InitializeComponent();
             this.DataContext = this;
 
+            this.GrainCount = "10";
+            this.StepsPerClick = "1";
             this.SpaceSize = "40";            
             this.Step = 0;
             StepLabel.Content = Step;
-
-
         }
 
        
         private void NextButton_Click(object sender, RoutedEventArgs e)
         {
-            if (_step == 0)
+            for(int k = 0; k < Convert.ToInt32(this.StepsPerClick); k++)
             {
-                this.automaton = new CelluralAutomaton(this._spaceSize);
-
-                this.redGrain = new Grain(0, System.Drawing.Color.Red);
-                this.blueGrain = new Grain(1, System.Drawing.Color.Blue);
-                this.cyanGrain = new Grain(2, System.Drawing.Color.Cyan);
-                this.magentaGrain = new Grain(3, System.Drawing.Color.Magenta);
-                this.yellowGrain = new Grain(4, System.Drawing.Color.Yellow);
-                this.greenGrain = new Grain(5, System.Drawing.Color.Green);
-                this.darkGreenGrain = new Grain(6, System.Drawing.Color.DarkGreen);
-
-                space = automaton.Space.currentState;
-                previousSpace = automaton.Space.lastState;
-
-                var r = new Random();
-
-                space[r.Next(0, this._spaceSize - 1), r.Next(0, this._spaceSize - 1)].GrainMembership = redGrain;
-                space[r.Next(0, this._spaceSize - 1), r.Next(0, this._spaceSize - 1)].GrainMembership = blueGrain;
-                space[r.Next(0, this._spaceSize - 1), r.Next(0, this._spaceSize - 1)].GrainMembership = cyanGrain;
-                space[r.Next(0, this._spaceSize - 1), r.Next(0, this._spaceSize - 1)].GrainMembership = magentaGrain;
-                space[r.Next(0, this._spaceSize - 1), r.Next(0, this._spaceSize - 1)].GrainMembership = yellowGrain;
-                space[r.Next(0, this._spaceSize - 1), r.Next(0, this._spaceSize - 1)].GrainMembership = greenGrain;
-                space[r.Next(0, this._spaceSize - 1), r.Next(0, this._spaceSize - 1)].GrainMembership = darkGreenGrain;
-
-                this.Grains_TextBox.IsEnabled = false;
-                this.SpaceSize_TextBox.IsEnabled = false;
-            }
-
-
-            PixelFormat pf = PixelFormats.Bgr32;
-            int width = this._spaceSize;
-            int height = this._spaceSize;
-            int rawStride = (width * pf.BitsPerPixel + 7) / 8;
-            byte[] rawImage = new byte[rawStride * height];
-            int rawImageIndex = 0;
-
-            // Initialize the image with data.
-            Random value = new Random();
-
-            
-            for (int i = 0; i < space.GetLength(0); i++)
-            {
-                for (int j = 0; j < space.GetLength(1); j++)
+                if (_step == 0)
                 {
-                    previousSpace[i, j] = space[i, j];
-                    space[i,j] = new Cell();
-                }
-            }
+                    this.automaton = new CelluralAutomaton(this._spaceSize);
 
-               
+                    grains = new Grain[Convert.ToInt32(GrainCount)];
+
+                    var r = new Random();
+
+                    space = automaton.Space.currentState;
+                    previousSpace = automaton.Space.lastState;
+
+                    for (int i = 0; i < this.grains.Length; i++)
+                    {
+                        this.grains[i] = new Grain(i, System.Drawing.Color.FromArgb(0, r.Next(0, 255), r.Next(0, 255), r.Next(0, 255))); // TODO: get random from HSV for fancy colors
+                        space[r.Next(0, this._spaceSize - 1), r.Next(0, this._spaceSize - 1)].GrainMembership = this.grains[i]; //TODO: check if not hiting occuppied cell
+                    }
+
+                    this.Grains_TextBox.IsEnabled = false;
+                    this.SpaceSize_TextBox.IsEnabled = false;
+                }
+
+
+                PixelFormat pf = PixelFormats.Bgr32;
+                int width = this._spaceSize;
+                int height = this._spaceSize;
+                int rawStride = (width * pf.BitsPerPixel + 7) / 8;
+                byte[] rawImage = new byte[rawStride * height];
+                int rawImageIndex = 0;
+
+                // Initialize the image with data.
+                Random value = new Random();
+
+
+                for (int i = 0; i < space.GetLength(0); i++)
+                {
+                    for (int j = 0; j < space.GetLength(1); j++)
+                    {
+                        previousSpace[i, j] = space[i, j];
+                        space[i, j] = new Cell();
+                    }
+                }
+
+
                 for (int i = 0; i < space.GetLength(0); i++)
                 {
                     for (int j = 0; j < space.GetLength(1); j++)
@@ -139,7 +135,7 @@ namespace MsmGrainGrowthGui
                         System.Drawing.Color pixelColor = space?[i, j]?.GrainMembership?.Color ?? System.Drawing.Color.White;
 
                         // write byte[index] with pixelColor
-                        if(rawImageIndex >= rawImage.Length)
+                        if (rawImageIndex >= rawImage.Length)
                         {
                             System.Diagnostics.Trace.WriteLine($"pixel [{i},{j}], rawIndex: {rawImageIndex}, outide of {rawImage.Length} bound");
                         }
@@ -148,24 +144,28 @@ namespace MsmGrainGrowthGui
                             rawImage[rawImageIndex++] = pixelColor.R;
                             rawImage[rawImageIndex++] = pixelColor.G;
                             rawImage[rawImageIndex++] = pixelColor.B;
-                            rawImage[rawImageIndex++] = pixelColor.A;
+                            rawImage[rawImageIndex++] = 0;
                         }
                     }
-                
-                
+
+
                 }
-            this.Step++;
-            this.StepLabel.Content = Step;
+                this.Step++;
+                this.StepLabel.Content = Step;
 
 
 
 
-            // Create a BitmapSource.
-            BitmapSource bitmap = BitmapSource.Create(width, height,
-                this._spaceSize, this._spaceSize, pf, null,
-                rawImage, rawStride);
+                // Create a BitmapSource.
+                BitmapSource bitmap = BitmapSource.Create(width, height,
+                    this._spaceSize, this._spaceSize, pf, null,
+                    rawImage, rawStride);
 
-            CelluralSpaceView.Source = bitmap;
+                CelluralSpaceView.Source = bitmap;
+            }
+
+
+            
             
         }
 
