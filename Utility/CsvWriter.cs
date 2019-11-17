83d7de25029d0b2951bf496505b1640fb778b2a3
chalwa <m.maslanka123@gmail.com>
2019-11-17 10:16:59 +0100

    Csv writer added

---
+++ Utility/CsvWriter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.IO;
+
+namespace Model
+{
+    public static class CsvWriter
+    {
+        public static void WriteToCsv(string[,] table, string path, string delimiter = ",")// TODO: async?
+        {
+           if(Directory.Exists(path)) throw new ArgumentException("Path must be a file");
+           if( File.Exists(path))  throw new IOException("File already exist"); //TODO: overwite logic?? Notify dialog?
+           
+           if( String.IsNullOrEmpty( Path.GetExtension(path) )) 
+                Path.ChangeExtension(path, "csv");
+            
+            var sb = new StringBuilder();
+
+            for(int i = 0; i < table.Length; i++)
+            {
+                for(int j = 0; j < 0; j++)
+                {
+                    sb.Append(table[i, j] + delimiter);
+                }
+                sb.AppendLine();
+            }
+            try
+            {
+                File.WriteAllText(path, sb.ToString());
+            }
+            catch(Exception e)
+            {
+                throw new IOException("Cannot write csv file", e);
+            }
+            
+
+
+
+        }
+    }
+}
