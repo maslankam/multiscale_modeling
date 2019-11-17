b2f4edf8f45f0e8399c234de6da278180886351b
chalwa <m.maslanka123@gmail.com>
2019-11-17 16:37:34 +0100

    X Y property of Cell

--- Model/Cell.cs
+++ Model/Cell.cs
@@ -4,11 +4,18 @@ using System.Drawing;
 namespace Model{    
     public class Cell
     {
-        public Grain GrainMembership;
+        public Grain GrainMembership { get; set; }
+        public int X { get; private set; }
+        public int Y { get; private set; }
 
         public Cell()
         {
             this.GrainMembership = null;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
         }
     }
 
